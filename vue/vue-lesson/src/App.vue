30d287566bf32efc1676c87b708267cd28c9a6af
cm118880 <mori.daiki@paypay-card.co.jp>
2024-10-27 12:47:05 +0900

    computedの学習

--- vue/vue-lesson/src/App.vue
+++ vue/vue-lesson/src/App.vue
@@ -3,14 +3,16 @@ import { ref, computed } from 'vue'
 const count = ref(0)
 const evaluation = computed(() => {
   console.log('computed evaluation')
-  count.value = 0
   return count.value > 3 ? 'Good' : 'Bad'
 })
 console.log('evaluation', evaluation.value)
+function getEvaluation() {
+  console.log(evaluation.value)
+}
 </script>
 <template>
   <p>{{ count > 3 ? 'Good' : 'Bad' }}</p>
-  <p>{{ evaluation }}</p>
   <p>{{ count }}</p>
   <button @click="count++">+1</button>
+  <button @click="getEvaluation">get Evaluation</button>
 </template>
